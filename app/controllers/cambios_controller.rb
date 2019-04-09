421b5dbd916dd0b577ed724c40347a7894c0fccb
guillermo varela <guillovarela16@gmail.com>
2019-04-09 12:13:02 -0300

    index view created

--- app/controllers/cambios_controller.rb
+++ app/controllers/cambios_controller.rb
@@ -1,2 +1,30 @@
 class CambiosController < ApplicationController
+
+  skip_before_action :authenticate_user!, only: :index
+  before_action :set_cambios, only: [:show, :edit, :update]
+  def index
+    @cambios = Cambio.all
+  end
+
+  def show
+
+  end
+
+  def edit
+  end
+
+  def update
+    @cambio.update(cambios_params)
+    redirect_to cambio_path(@cambio)
+  end
+
+  private
+
+  def cambios_params
+    params.require(:cambio).permit(:dolarv, :dolarc, :euroc, :eurov, :realc, :realv)
+  end
+
+  def set_cambios
+    @cambio = Cambio.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'cambios#index'
+  resources :cambios, only: [:index, :show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
