37f717953829feeffaf732a200e91f38ec58fc42
guillermo varela <guillovarela16@gmail.com>
2019-08-20 14:55:30 -0300

    add libras cambio

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_20_175314) do
+ActiveRecord::Schema.define(version: 2019_08_20_175453) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2019_08_20_175314) do
     t.string "chilenoc"
     t.string "chilenov"
     t.string "gbpcompra"
+    t.string "gbpventa"
   end
 
   create_table "users", force: :cascade do |t|
