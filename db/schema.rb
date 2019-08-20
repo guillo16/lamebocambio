b56c7dcb85c5d51b191ccf33715aad2f325ced8a
guillermo varela <guillovarela16@gmail.com>
2019-08-20 14:54:02 -0300

    libras added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_04_145433) do
+ActiveRecord::Schema.define(version: 2019_08_20_175314) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2019_06_04_145433) do
     t.string "uruguayov"
     t.string "chilenoc"
     t.string "chilenov"
+    t.string "gbpcompra"
   end
 
   create_table "users", force: :cascade do |t|
